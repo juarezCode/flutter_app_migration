295cbfc548ac6a2d6a2b7cac3c65aec30fde5ca6
Roberto Juarez <rjuarez@mediomelon.mx>
2021-08-30 17:52:51 -0500

    Flutter fix primera parte

--- lib/src/models/actores_model.dart
+++ lib/src/models/actores_model.dart
@@ -1,22 +1,15 @@
 class Cast {
+  List<Actor> actores = [];
 
-  List<Actor> actores = new List();
+  Cast.fromJsonList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
 
-
-  Cast.fromJsonList( List<dynamic> jsonList  ){
-
-    if ( jsonList == null ) return;
-
-    jsonList.forEach( (item) {
+    jsonList.forEach((item) {
       final actor = Actor.fromJsonMap(item);
       actores.add(actor);
     });
   }
-
 }
-
-
-
 
 class Actor {
   int castId;
@@ -39,27 +32,22 @@ class Actor {
     this.profilePath,
   });
 
-  Actor.fromJsonMap( Map<String, dynamic> json ) {
-
-    castId      = json['cast_id'];
-    character   = json['character'];
-    creditId    = json['credit_id'];
-    gender      = json['gender'];
-    id          = json['id'];
-    name        = json['name'];
-    order       = json['order'];
+  Actor.fromJsonMap(Map<String, dynamic> json) {
+    castId = json['cast_id'];
+    character = json['character'];
+    creditId = json['credit_id'];
+    gender = json['gender'];
+    id = json['id'];
+    name = json['name'];
+    order = json['order'];
     profilePath = json['profile_path'];
-
   }
 
-   getFoto() {
-
-    if ( profilePath == null ) {
+  getFoto() {
+    if (profilePath == null) {
       return 'http://forum.spaceengine.org/styles/se/theme/images/no_avatar.jpg';
     } else {
       return 'https://image.tmdb.org/t/p/w500/$profilePath';
     }
-
   }
-
 }
--- lib/src/models/pelicula_model.dart
+++ lib/src/models/pelicula_model.dart
@@ -1,26 +1,19 @@
 class Peliculas {
-
-  List<Pelicula> items = new List();
+  List<Pelicula> items = [];
 
   Peliculas();
 
-  Peliculas.fromJsonList( List<dynamic> jsonList  ) {
+  Peliculas.fromJsonList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
 
-    if ( jsonList == null ) return;
-
-    for ( var item in jsonList  ) {
+    for (var item in jsonList) {
       final pelicula = new Pelicula.fromJsonMap(item);
-      items.add( pelicula );
+      items.add(pelicula);
     }
-
   }
-
 }
 
-
-
 class Pelicula {
-
   String uniqueId;
 
   int voteCount;
@@ -55,48 +48,36 @@ class Pelicula {
     this.releaseDate,
   });
 
-  Pelicula.fromJsonMap( Map<String, dynamic> json ) {
-
-    voteCount        = json['vote_count'];
-    id               = json['id'];
-    video            = json['video'];
-    voteAverage      = json['vote_average'] / 1;
-    title            = json['title'];
-    popularity       = json['popularity'] / 1;
-    posterPath       = json['poster_path'];
+  Pelicula.fromJsonMap(Map<String, dynamic> json) {
+    voteCount = json['vote_count'];
+    id = json['id'];
+    video = json['video'];
+    voteAverage = json['vote_average'] / 1;
+    title = json['title'];
+    popularity = json['popularity'] / 1;
+    posterPath = json['poster_path'];
     originalLanguage = json['original_language'];
-    originalTitle    = json['original_title'];
-    genreIds         = json['genre_ids'].cast<int>();
-    backdropPath     = json['backdrop_path'];
-    adult            = json['adult'];
-    overview         = json['overview'];
-    releaseDate      = json['release_date'];
-
-
+    originalTitle = json['original_title'];
+    genreIds = json['genre_ids'].cast<int>();
+    backdropPath = json['backdrop_path'];
+    adult = json['adult'];
+    overview = json['overview'];
+    releaseDate = json['release_date'];
   }
 
   getPosterImg() {
-
-    if ( posterPath == null ) {
+    if (posterPath == null) {
       return 'https://cdn11.bigcommerce.com/s-auu4kfi2d9/stencil/59512910-bb6d-0136-46ec-71c445b85d45/e/933395a0-cb1b-0135-a812-525400970412/icons/icon-no-image.svg';
     } else {
       return 'https://image.tmdb.org/t/p/w500/$posterPath';
     }
-
   }
 
   getBackgroundImg() {
-
-    if ( posterPath == null ) {
+    if (posterPath == null) {
       return 'https://cdn11.bigcommerce.com/s-auu4kfi2d9/stencil/59512910-bb6d-0136-46ec-71c445b85d45/e/933395a0-cb1b-0135-a812-525400970412/icons/icon-no-image.svg';
     } else {
       return 'https://image.tmdb.org/t/p/w500/$backdropPath';
     }
-
   }
-
-
-
 }
-
-
